9fd4b81ef1cf7e4373d57cf45403efa2c0dc80bf
pritam04mandal <pritammandalvnbs@gmail.com>
2025-07-08 10:30:24 +0530

    fix: changes in the config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,5 +8,4 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar.log
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
-polybar second 2>&1 | tee -a /tmp/polybar.log & disown
 echo "Bars launched..."
